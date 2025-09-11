2b008aa883e559194beafd07b253d733b6db36a5
davijr78 <davi.starepravo.jr@outlook.com>
2025-09-11 13:53:18 -0300

    prettier code

--- refactoredCode.py
+++ refactoredCode.py
@@ -2,12 +2,13 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from matplotlib import cm  # Color map
+from matplotlib import cm
+
 matplotlib.use('Agg')
 
 os.makedirs("plots", exist_ok=True)
 
-def linear_system_2d_2nd_order(n, maxVal):
+def matrix_2ndOrder(n, maxVal):
     h = maxVal/(n-1)
     size = n*n
     A = np.zeros((size, size))
@@ -25,12 +26,10 @@ def linear_system_2d_2nd_order(n, maxVal):
                 A[idx, idx + 1] = 1  # right
     return A / h**2
 
-
-def linear_system_2d_4th_order(n, maxVal):
+def matrix_4thOrder(n, maxVal):
     h = maxVal / (n-1)
     size = n*n
     A = np.zeros((size, size))
-
     for i in range(n):
         for j in range(n):
             idx = i * n + j
@@ -55,7 +54,7 @@ def linear_system_2d_4th_order(n, maxVal):
                 A[idx, idx + 2] = -1  # 2 right
     return A / (12 * h ** 2)
 
-def rhs_2ndOrder(n, maxVal, A):
+def rhs_2ndOrder(n, maxVal):
     b = np.zeros((n, n))
     h = maxVal / (n-1)
     for i in range(n):
@@ -73,8 +72,8 @@ def rhs_2ndOrder(n, maxVal, A):
             if i == 0 or i == n - 1 or j == 0 or j == n - 1:
                 b[i, j] = -4 * (np.pi**2) * (np.sin(2 * np.pi * x) + np.sin(2 * np.pi * y)) - (np.sin(2 * np.pi * x) + np.sin(2 * np.pi * y)) / h**2
     return b
-
-def rhs_4thOrder(n, maxVal, A):
+    
+def rhs_4thOrder(n, maxVal):
     b = np.zeros((n, n))
     h = maxVal / (n-1)
     for i in range(n):
@@ -115,6 +114,37 @@ def rhs_4thOrder(n, maxVal, A):
 
     return b
 
+def matrix_setup(n, maxVal, order=4):
+    if order == 2:
+        return matrix_2ndOrder(n, maxVal)
+    elif order == 4:
+        return matrix_4thOrder(n, maxVal)
+    else:
+        print("order not yet programed")
+        return None
+
+def rhs_setup(n, maxVal, A, order=4):
+    if order == 2:
+        return rhs_2ndOrder(n, maxVal)
+    elif order == 4:
+        return rhs_4thOrder(n, maxVal)
+    else:
+        print("order not yet programed")
+        return None
+
+def numeric_solution_setup(n, maxVal, order=4):
+    if order == 2:
+        A = matrix_setup(n, maxVal, order)
+        b = rhs_setup(n, maxVal, order)
+        return A, b
+    if order == 4:
+        A = matrix_setup(n, maxVal, order)
+        b = rhs_setup(n, maxVal, order)
+        return A, b
+    else:
+        print("order not setup yet!")
+        return None
+        
 def plot_solution(U, X, Y, title):
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111, projection='3d')
@@ -127,158 +157,81 @@ def plot_solution(U, X, Y, title):
     plt.savefig(f"plots/{title}.png", dpi=300, bbox_inches='tight')
     plt.close()
 
-def iterations(resolutions, max_val):
-
-    errors_2ndOrder = []
-    errors_4thOrder = []
-    hs = []
-
-    for res in resolutions:
-        # Initilizing Libnear System
-        A_2nd_order = linear_system_2d_2nd_order(res, max_val)
-        A_4th_order = linear_system_2d_4th_order(res, max_val)
-        rhs_2nd_Order = rhs_2ndOrder(res, max_val, A_2nd_order)
-        rhs_4th_Order = rhs_4thOrder(res, max_val, A_4th_order)
-
-        # Setting up analytic solution
-        x = np.linspace(0,max_val,points)
-        y = np.linspace(0,max_val,points)
-        X,Y = np.meshgrid(x,y)
-        anlytc_solution = lambda x, y: np.sin(2 * np.pi * x) + np.sin(2 * np.pi * y)
-
-        # Calculating Solution
-        sol_2ndOrder = np.linalg.solve(A_2nd_order, rhs_2nd_Order.flatten())
-        sol_4thOrder = np.linalg.solve(A_4th_order, rhs_4th_Order.flatten())
-
-        # Making Graphs
-        x = np.linspace(0, max_val, res)
-        y = np.linspace(0, max_val, res)
-        X, Y = np.meshgrid(x, y)
-
-        U_4th = sol_4thOrder.reshape((res, res))
-        fig = plt.figure(figsize=(10, 7))
-        ax = fig.add_subplot(111, projection='3d')
-        surf = ax.plot_surface(X, Y, U_4th, cmap=cm.viridis)
-
-        plot_solution(U_4th, X, Y, f"4th Order Numeric Solution Plot-{res}")
-
-        U_2nd = sol_2ndOrder.reshape((res, res))
-        fig = plt.figure(figsize=(10, 7))
-        ax = fig.add_subplot(111, projection='3d')
-        surf = ax.plot_surface(X, Y, U_2nd, cmap=cm.viridis)
-
-        plot_solution(U_2nd, X, Y, f"2nd Order Numeric Solution Plot-{res}")
-
-        # Computing errors
-        err_2ndOrder = compute_error(sol_2ndOrder, np.array(anlytc_solution(X,Y)).flatten())
-        err_4thOrder = compute_error(sol_4thOrder, np.array(anlytc_solution(X,Y)).flatten())
-        hs.append(max_val / (res-1))
-        errors_2ndOrder.append(err_2ndOrder)
-        errors_4thOrder.append(err_4thOrder)
-
-    return hs, errors_2ndOrder, errors_4thOrder, sol_2ndOrder, sol_4thOrder
-
 def compute_error(U_numeric, U_exact):
     return np.sqrt(np.mean((U_numeric - U_exact)**2))
 
-def perfect_solution(n, max_val, plot=True):
+def perfect_solution(n, max_val, plot=False):
     analytic_solution = lambda x, y: np.sin(2 * np.pi * x) + np.sin(2 * np.pi * y)
     if plot:
-        x = np.linspace(0,max_val,points)
+        x = np.linspace(0,max_val,n)
         y = np.linspace(0,max_val,points)
         X,Y = np.meshgrid(x,y)
         U = analytic_solution(X,Y)
         plot_solution(U, X, Y, "Analytical Solution Surface Plot")
         return U,X,Y
     else:
-        x = np.linspace(0,max_val,points)
-        y = np.linspace(0,max_val,points)
+        x = np.linspace(0,max_val,n)
+        y = np.linspace(0,max_val,n)
         X,Y = np.meshgrid(x,y)
         U = analytic_solution(X,Y)
         return U,X,Y
 
-def simple_solution(n, max_val, order=4, plot=True):
-    if order == 2:
+def error_analysis(resolutions, maxVal, plot=False):
+    errors_2nd = []
+    errors_4th = []
+    hs = []
+
+    for res in resolutions:
+        A_2nd, b_2nd = numeric_solution_setup(res, maxVal, order=2)
+        A_4th, b_4th = numeric_solution_setup(res, maxVal, order=4)
+        U,X,Y = perfect_solution(res, maxVal)
+
+        solution_2nd = np.linalg.solve(A_2nd, b_2nd.flatten())
+        U_2nd = solution_2nd.reshape((res,res))
+        solution_4th = np.linalg.solve(A_4th, b_4th.flatten())
+        U_4th = solution_4th.reshape((res,res))
+
         if plot:
-            A = linear_system_2d_2nd_order(n, max_val)
-            b = rhs_2ndOrder(n, max_val, A)
+            plot_solution(U_2nd, X, Y, f"Numeric Solution (2nd Order) ({res})")
+            plot_solution(U_4th, X, Y, f"Numeric Solution (4th Order) ({res})")
+                
+        err_2nd = compute_error(solution_2nd, np.array(U).flatten())
+        err_4th = compute_error(solution_4th, np.array(U).flatten())
+        hs.append(maxVal / (res-1))
+        errors_2nd.append(err_2nd)
+        errors_4th.append(err_4th)
 
-            x = np.linspace(0,max_val,n)
-            y = np.linspace(0,max_val,n)
-            X,Y = np.meshgrid(x,y)
-            solution = np.linalg.solve(A, b.flatten())
-            U = solution.reshape((n, n))
-            
-            plot_solution(U, X, Y, f"Numeric Solution Plot-{points}")
+    return hs, errors_2nd, errors_4th
 
-            return U,X,Y
-
-        else:
-            A = linear_system_2d_4th_order(n, max_val)
-            b = rhs_4thOrder(n, max_val, A)
-
-            x = np.linspace(0,max_val,n)
-            y = np.linspace(0,max_val,n)
-            X,Y = np.meshgrid(x,y)
-            solution = np.linalg.solve(A, b.flatten())
-            U = solution.reshape((n, n))
-          
-            return U,X,Y
-
-    if order == 4:
-        if plot:
-            A = linear_system_2d_4th_order(n, max_val)
-            b = rhs_4thOrder(n, max_val, A)
-
-            x = np.linspace(0,max_val,n)
-            y = np.linspace(0,max_val,n)
-            X,Y = np.meshgrid(x,y)
-            solution = np.linalg.solve(A, b.flatten())
-            U = solution.reshape((n, n))
-
-            plot_solution(U, X, Y, f"Analytic Solution Plot 4th-{points}")
-
-            return U,X,Y
-        
-        else:
-            A = linear_system_2d_4th_order(n, max_val)
-            b = rhs_4thOrder(n, max_val, A)
-
-            x = np.linspace(0,max_val,n)
-            y = np.linspace(0,max_val,n)
-            X,Y = np.meshgrid(x,y)
-            solution = np.linalg.solve(A, b.flatten())
-            U = solution.reshape((n, n))
-
-            return U,X,Y
-
-    else:
-        print("order not yet programed")
+def covergence_analysis(res, hs, errs2, errs4, writeOut=True, plot=True):
+    if writeOut:
+        for i in range(len(res)):
+            print(f"2nd order error for resolution: {res[i]}\n{errs2[i]}")
+            print(f"4th order error for resolution: {res[i]}\n{errs4[i]}")
+            print("")
+    if plot:
+        plt.figure(figsize=(8, 6))
+        plt.loglog(hs, errs2, 'o-', label='2nd Order Error')
+        plt.loglog(hs, errs4, 's-', label='4th Order Error')
+        plt.xlabel('Grid Spacing (h)')
+        plt.ylabel('L2 Error')
+        plt.title('Error vs Grid Spacing (Log-Log Plot)')
+        plt.grid(True, which="both", ls="--")
+        plt.legend()
+        plt.savefig("plots/error_convergence_plot.png", dpi=300, bbox_inches='tight')
+        plt.close()
 
 
-if __name__ == '__main__':
-    points = 10
+if __name__ == "__main__":
     max_val = 1
-    perfect_solution(points, max_val)
-    simple_solution(points, max_val, order=4)
+    resolutions = [10, 20, 40, 60, 80]
+    hs, errs2, errs4 = error_analysis(resolutions, max_val, plot=True)
+    covergence_analysis(resolutions, hs, errs2, errs4)
+    
 
-    # Running through iterations to check convergence
-    res = [10, 20, 40, 60]
-    hs, errs2, errs4, sol2nd, sol4th = iterations(res, max_val=max_val)
-    for i in range(len(res)):
-        print(f"2nd order error for resolution: {res[i]}\n{errs2[i]}")
-        print(f"4th order error for resolution: {res[i]}\n{errs4[i]}")
-        print("")
 
-    # Plotting error convergence
-    plt.figure(figsize=(8, 6))
-    plt.loglog(hs, errs2, 'o-', label='2nd Order Error')
-    plt.loglog(hs, errs4, 's-', label='4th Order Error')
-    plt.xlabel('Grid Spacing (h)')
-    plt.ylabel('L2 Error')
-    plt.title('Error vs Grid Spacing (Log-Log Plot)')
-    plt.grid(True, which="both", ls="--")
-    plt.legend()
-    plt.savefig("plots/error_convergence_plot.png", dpi=300, bbox_inches='tight')
-    plt.close()
+
+
+    
+
 
